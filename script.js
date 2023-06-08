6ee974f073950031d7f51683b528efc869a8db6d
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2023-06-08 17:57:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let list = document.getElementsByTagName("li");
+for(let i = 0; i < list.length; i++){
+    if(list[i].id === "level"){
+        alert("The level of the element is:"+list[i].innerText);
+    }
+}
